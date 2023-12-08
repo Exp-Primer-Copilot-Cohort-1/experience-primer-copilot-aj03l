d96d44e5dfbdfc3687e7a42fb0d134621b196569
Joel Osagumwenro <121755148+aj03l@users.noreply.github.com>
2023-12-08 10:53:28 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    var skills = [
+        { name: 'HTML', level: 10 },
+        { name: 'CSS', level: 8 },
+        { name: 'JS', level: 9 },
+        { name: 'PHP', level: 7 },
+        { name: 'SQL', level: 6 },
+        { name: 'C#', level: 5 }
+    ];
+
+    var member = {
+        name: 'John',
+        age: 26,
+        skills: skills
+    };
+
+    return member;
+}
